aba6cff9bbf88b9c0fa0c8cc1ab2df6912c81ffc
Cristina Swearingen <criss.foltea@yahoo.com>
2015-07-27 12:29:15 -0600

    change

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true, cookie: { secure: true } }))
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true}))
 app.use(passport.initialize());
 app.use(passport.session());
 
